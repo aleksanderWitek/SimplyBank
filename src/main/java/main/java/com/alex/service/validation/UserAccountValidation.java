7e4d002679726c660c60e6d8090d5afc7641dad4
aleks <aleksanderwitek95@gmail.com>
2026-01-28 00:01:15 +0000

    Work in progress. Created validation for UserAccountService.

--- src/main/java/main/java/com/alex/service/validation/UserAccountValidation.java
+++ src/main/java/main/java/com/alex/service/validation/UserAccountValidation.java
@@ -1,20 +1,27 @@
 package main.java.com.alex.service.validation;
 
 import main.java.com.alex.UserAccountRole;
+import main.java.com.alex.dto.UserAccount;
 import main.java.com.alex.exception.IllegalArgumentRuntimeException;
 import main.java.com.alex.exception.NullPointerRuntimeException;
 
 public class UserAccountValidation {
 
-    public static void validateIfUserAccountRoleIsCorrect(String userAccountRole, String message){
+    public static void ensureUserAccountPresent(UserAccount userAccount) {
+        if(userAccount == null) {
+            throw new IllegalArgumentRuntimeException("UserAccount is null");
+        }
+    }
+
+    public static void validateIfUserAccountRoleIsCorrect(String userAccountRole){
         if (userAccountRole == null || userAccountRole.isBlank()) {
-            throw new NullPointerRuntimeException(message);
+            throw new NullPointerRuntimeException("User Account Role is null or empty");
         }
 
         try {
             UserAccountRole.valueOf(userAccountRole.toUpperCase());
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentRuntimeException(message);
+            throw new IllegalArgumentRuntimeException("userAccountRole is invalid: " + userAccountRole);
         }
     }
 }
